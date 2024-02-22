7929be6d79db9f717b5ba7cddfd41435096efd48
Varshita-Tummuri <155758725+Varshita-Tummuri@users.noreply.github.com>
2024-02-22 22:40:36 +0530

    Update polindrame.c

--- polindrame.c
+++ polindrame.c
@@ -1,16 +1,25 @@
-//show polindrame of a number
+//show whether the number is polindrame or not
 #include <stdio.h>
-int main()
-{
-	int x,y,temp;
-	printf("Enter a number: ");
-	scanf("%d",&x);
-	if(x!=0)
-	{
-		y=x%100;
-		temp=x-y;
-		x=temp/10;
+int main() {
+    int x, y,a, temp = 0;
+    printf("Enter a number: ");
+    scanf("%d", &x);
+	a=x;
+    while (x != 0) 
+	{	//x=123
+        y = x % 10;//y=3
+        temp = temp * 10 + y;//temp=3
+        x = x / 10;//x
+    }
+    printf("The reverse of %d is: %d\n",a, temp);
+    if(a==temp)
+    {
+    	printf("This number is a palindrome!");
 	}
-	printf("The REVESE IS %d",)
-	return 0;
+	else
+	{
+		printf("It is not a palindrome number.");
+	}
+    return 0;
 }
+
