e0e9728a11f6313c548339c8221c011bef88fdcd
Varshita-Tummuri <155758725+Varshita-Tummuri@users.noreply.github.com>
2024-02-27 14:00:10 +0530

    Update sum_of_2Matrices.c

--- sum_of_2Matrices.c
+++ sum_of_2Matrices.c
@@ -1,15 +1,16 @@
-/*2D arrays*/
+//sum of two matrices
 #include<stdio.h>
 int main()
 {
-	int r,c,i,j,x,y,v,t;
+	int i,j,r,c;
 	int a[10][10];
 	int b[10][10];
 	int s[10][10];
 	printf("Enter row size:");
 	scanf("%d",&r);
-	printf("\nEnter column size:");
+	printf("Enter column size:");
 	scanf("%d",&c);
+	
 	printf("\nEnter the elemnts for matrix 1:");
 	for(i=0;i<r;i++)
 	{
@@ -18,23 +19,52 @@ int main()
 			scanf("%d",&a[i][j]);
 		}
 	}
-	printf("\nEnter the elements for matrix 2:");
-	for(x=0;x<r;x++)
+	printf("\nThe elemnts for matrix 1 are: \n");
+	for(i=0;i<r;i++)
 	{
-		for(y=0;y<c;y++)
+		for(j=0;j<c;j++)
 		{
-			scanf("%d",&b[x][y]);
+			printf("%d\t",a[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("\nEnter the elements for matrix 2:");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&b[i][j]);
 		}
 	}
-	/*for(v=0;v<r;v++)
+	printf("\nThe elemnts for matrix 2 are:\n");
+	for(i=0;i<r;i++)
 	{
-		for(t=0;t<c;t++)
+		for(j=0;j<c;j++)
 		{
-		
+			printf("%d\t",b[i][j]);
 		}
-	}*/
+		printf("\n");
+	}
 	
-	printf("%d",a[i][j]);
+//for sum of the matrices
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			s[i][j]=a[i][j]+b[i][j];
+		}
+	}
+	
+	printf("\nThe sum of the two matrices is:\n ");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("%d\t",s[i][j]);
+		}
+		printf("\n");
+	}
 	return 0;
-	
+
 }
